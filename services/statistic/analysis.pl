2589c8c63f26d3bfae91ddb6ce80aa59bcaf4100
yangsongxiang <yangsongxiang@caredear.com>
2014-11-26 15:25:44 +0800

    (Bug 3969) Stage2 of statistic code on server
    Change-Id: I926aa2a3e71e0d614cad1c69d8ff1ddb864d30d2

--- services/statistic/analysis.pl
+++ services/statistic/analysis.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl
-
+use Encode;
+use LWP;
+use LWP::UserAgent;
+use LWP::Simple;
+use HTTP::Cookies;
 ##
 # Perl script to parse user text data
 # and give some statistic report
@@ -21,34 +25,78 @@ $FULLUSER = "fulluser.txt";
 # stored the latest mobile=>Province into .csv as preloaded for
 # next time's parsing.
 sub update_csvdata() {
+    my ($myl) = (@_);
+    # remmember to encode it as UTF-8
+    open(CSV, ">:encoding(utf-8)", "$PRELOAD") or die "failed try writing $PRELOAD\n";
+    foreach $k (keys $myl){
+      $v = ${$myl}{$k};
+      $val = decode("gbk", $v);
+      print CSV "$k,$val\n";
+    }
+    close(CSV);
 }
 
 # try fill up the num_list, and store it into a hash
 sub get_num_list() {
-    my ($l) = (@_); # input param is actually a reference of Hash!
+    my ($myh) = (@_); # input param is actually a reference of Hash!
 
     my $UA = LWP::UserAgent->new(keep_alive=>1);
     $UA->agent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36");
 
     #First try get all existed records from a preload csv file.
-    open(CSV, $PRELOAD) || die "failed open preload csv";
+    open(CSV, "$PRELOAD") || die "failed open preload csv";
     while(<CSV>){
         @lines = split(",");
-        $l{$lines[0]} = $lines[1];
+        ${$myh}{$lines[0]} = $lines[1];
     }
     close(CSV);
+
+    printf("Finished the preload CSV reading...\n");
 
     #next, read fulluser data file...
     open(FULL, $FULLUSER) || die "failed open full user data";
     while(<FULL>){
         $line = $_;
         chomp($line); # here store one mobile number, like 13022592323
-        unless (exists $l{$line}) {
+        printf("the phone number=$line\n");
+        unless (exists ${$myh}{$line}) {
             printf("$line didn't existed in the preload.csv, will get it from internet...\n");
             my $url = $LEADING_URL . "mobile=" . $line . "&action=mobile";
+
             my $res = $UA->get($url);
             if($res->is_success) {
                 my $html = $res->content;
+                open(TMPFILE, ">", "get.html") or die "failed write the html";
+                print TMPFILE $html;
+                close(TMPFILE);
+
+                open(TMPFILE, "get.html") || die "failed open the temp html";
+                $TXT_ANCHOR = "ø®∫≈πÈ Ùµÿ";
+                while(<TMPFILE>) {
+                    if($_ =~ /\Q$TXT_ANCHOR\E.*class=tdc2\>\<.*--\>(.*)\<\/TD\>/) {
+                        $h = $1;
+                        if($h =~ /(.*)\&nbsp;(.*)/) {
+                            ${$myh}{$line} = $1;
+                            $o = ${$myh}{$line};
+                            $out = decode("gbk", $o);
+                            $o = encode("utf-8", $out);
+                            printf("\t$line ==> $o\n");
+                            last; #remember , last is C's break;
+                        }
+                    } elsif($_ =~ /noswap\>\Q$TXT_ANCHOR\E\<\/TD\>/ || $_ =~ /\Q$TXT_ANCHOR\E/) {
+                        $newline = 1;#result stored at next new line
+                    } elsif($newline == 1) {
+                        $newline = 0;
+                        if($_ =~ /\>(.*)\&nbsp;(.*)\<\/TD\>/){
+                            ${$myh}{$line} = $1;
+                            $o = ${$myh}{$line};
+                            $out = decode("gbk", $o);
+                            $o = encode("utf-8", $out);
+                            printf("\t$line ==> $o\n");
+                        }
+                    }
+                }
+                close(TMPFILE);
             }
         }
     }
@@ -81,19 +129,9 @@ our %num_list;
 our %prov_list;
 
 &get_num_list(\%num_list);
+$len = keys %num_list;
+printf("After call get_num_list, the whole users info account = $len\n");
 
+&update_csvdata(\%num_list);
+printf("All user data stored in $PRELOAD\n");
 
-#####ENDOF====
-my %name;
-
-$name{13815882359} = "Jiangsu";
-$name{13022593515} = "BeiJing";
-
-foreach $n (keys %name) {
-    printf("$n ==> $name{$n}\n");
-}
-
-@keys = keys %name;
-@all = values %name;
-
-print @all;
