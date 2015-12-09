c8d9afceab49c6c74a775ceedac6d2ba17780382
Yang Songxiang <yangsongxiang@caredear.com>
2015-12-09 14:45:16 +0800

    (Bug 8858) Add OpenCV to the repo
    Change-Id: I781f4821d362bdd39c75475e6b4d0b371ace4733

---
+++ 3rd_party/opencv/opencv3.0-android-SDK/samples/face-detection/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,166 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/cvdef.h \
+ ../../sdk/native/jni/include/opencv2/hal/defs.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ ../../sdk/native/jni/include/opencv2/core/base.hpp \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/climits \
+ ../../sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstring \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cctype \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/string \
+ ../../sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ ../../sdk/native/jni/include/opencv2/hal.hpp \
+ ../../sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ ../../sdk/native/jni/include/opencv2/core/traits.hpp \
+ ../../sdk/native/jni/include/opencv2/core/matx.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types.hpp \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cfloat \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ ../../sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ ../../sdk/native/jni/include/opencv2/core/persistence.hpp \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstdio \
+ ../../sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ ../../sdk/native/jni/include/opencv2/core/utility.hpp \
+ ../../sdk/native/jni/include/opencv2/core/core_c.h \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/assert.h \
+ ../../sdk/native/jni/include/opencv2/core/optim.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ /home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/cvdef.h:
+
+../../sdk/native/jni/include/opencv2/hal/defs.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+../../sdk/native/jni/include/opencv2/core/base.hpp:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/climits:
+
+../../sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstring:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cctype:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/string:
+
+../../sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+../../sdk/native/jni/include/opencv2/hal.hpp:
+
+../../sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+
+../../sdk/native/jni/include/opencv2/core/traits.hpp:
+
+../../sdk/native/jni/include/opencv2/core/matx.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types.hpp:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cfloat:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+../../sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+../../sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstdio:
+
+../../sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+../../sdk/native/jni/include/opencv2/core/utility.hpp:
+
+../../sdk/native/jni/include/opencv2/core/core_c.h:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/assert.h:
+
+../../sdk/native/jni/include/opencv2/core/optim.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/home/yang/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/home/yang/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/log.h:
