403c637a90ac4d7c9215f6afe23b65a9ab9484b1
Yang Songxiang <yangsongxiang@caredear.com>
2015-12-16 10:25:13 +0800

    (Bug 8858) A whole process for predict article type
    Change-Id: I3814a096bb7a351e81f56d2fd3e7910abe204d7b

--- ml/autolabel.py
+++ ml/autolabel.py
@@ -1,2 +1,82 @@
-#!/usr/bin/python
+#coding=utf-8
+import sys, os, re
+import numpy
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
 
+def predict_with_bayes():
+    # create the pipline for Classifier
+    txt_clf = Pipeline(
+            [
+                ('vect', CountVectorizer()),
+                ('tfidf', TfidfTransformer()),
+                ('clf', MultinomialNB()),
+            ])
+    return 0
+
+def _read_data_content(datadir, fnamelist):
+    cat_idx = []
+    cat_content = []
+
+
+    for it in fnamelist:
+        t = 0
+        content = ''
+        pattern = re.compile(r'(\d)\_\d*.txt')
+        match = pattern.findall(it)
+        t = int(match[0])
+        fmt = '%s/%s' %(datadir, it)
+        rdfile = open(fmt)
+        while 1:
+            line = rdfile.readline()
+            if not line:
+                break
+            content += line
+        cat_idx.append(t)
+        cat_content.append(content)
+        print '%d --> %s' %(t, 'sth')
+
+    return (cat_idx, cat_content)
+
+# try loading all traning data under 'data/' dir
+def load_training_data(datadir):
+    cat_idx = []
+    cat_content = []
+
+    for it in os.walk(datadir):
+        print 'one step traverse'
+        # FIXME - the it[2] store all list file...
+        (cat_idx, cat_content) = _read_data_content(datadir, it[2])
+
+    # next try learning it
+    print 'prepare traning...'
+    txt_clf = Pipeline(
+            [
+                ('vect', CountVectorizer()),
+                ('tfidf', TfidfTransformer()),
+                ('clf', MultinomialNB()),
+            ])
+    txt_clf = txt_clf.fit(cat_content, cat_idx)
+    print 'finished traning...'
+
+    # try predict a new text paragraph
+    for it in os.walk('compare_data/'):
+        # FIXME - the it[2] store all list file...
+        (c_idx, c_content) = _read_data_content(datadir, it[2])
+    predicted = txt_clf.predict(c_content)
+    print predicted
+
+    print numpy.mean(predicted == c_idx)
+
+    print(metrics.classification_report(c_idx, predicted, c_idx))
+
+    return 0
+
+#
+# Main Entry Point:
+load_training_data('data/')
--- ml/fetchdata.py
+++ ml/fetchdata.py
@@ -24,7 +24,7 @@ def _extract_plain_text(cat, article_id):
     js_data = json.loads(req.read())
     return js_data['data']
 
-def store_text(cat, response_text):
+def store_text(cat, response_text, compare = 0):
     idx = 0
     name = ''
     fulldata = '' # include title + body
@@ -32,7 +32,11 @@ def store_text(cat, response_text):
     for it in response_text:
         print it['id']
         idx += 1
-        name = 'data/%d_%d.txt' %(cat, idx)
+        if compare == 0:
+            name = 'data/%d_%d.txt' %(cat, idx)
+        else:
+            name = 'compare_data/%d_%d.txt' %(cat, idx)
+
         fulldata = '%s\n%s' %(it['title'], _extract_plain_text(cat, it['id']))
         _write_data_into_file(name, fulldata)
 
@@ -49,7 +53,47 @@ def get_all_news(category):
     store_text(category, js_data['post'])
     return 0
 
+def prepare_compare_data():
+    param = ({'utype':2,'uid':'imei','category':1,'offset':102, 'count':4})
+    req_url = default_server + '/info/'
+    print 'the req url is %s' %(req_url)
+    client = urllib2.Request(req_url)
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    response = opener.open(client, json.JSONEncoder().encode(param))
+    js_data = json.loads(response.read())
+    store_text(1, js_data['post'], 1) # 1 means store into a compare data dir
+
+    param = ({'utype':2,'uid':'imei','category':2,'offset':102, 'count':4})
+    req_url = default_server + '/info/'
+    print 'the req url is %s' %(req_url)
+    client = urllib2.Request(req_url)
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    response = opener.open(client, json.JSONEncoder().encode(param))
+    js_data = json.loads(response.read())
+    store_text(2, js_data['post'], 1) # 1 means store into a compare data dir
+
+    param = ({'utype':2,'uid':'imei','category':3,'offset':102, 'count':4})
+    req_url = default_server + '/info/'
+    print 'the req url is %s' %(req_url)
+    client = urllib2.Request(req_url)
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    response = opener.open(client, json.JSONEncoder().encode(param))
+    js_data = json.loads(response.read())
+    store_text(3, js_data['post'], 1) # 1 means store into a compare data dir
+
+    param = ({'utype':2,'uid':'imei','category':4,'offset':102, 'count':4})
+    req_url = default_server + '/info/'
+    print 'the req url is %s' %(req_url)
+    client = urllib2.Request(req_url)
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+    response = opener.open(client, json.JSONEncoder().encode(param))
+    js_data = json.loads(response.read())
+    store_text(4, js_data['post'], 1) # 1 means store into a compare data dir
+    return 0
+
 ## Main Entry:
-alltypes = [1, 2, 3, 4]
-for i in alltypes:
-    get_all_news(i)
+#alltypes = [1, 2, 3, 4]
+#for i in alltypes:
+#    get_all_news(i)
+# prepare some new data...
+prepare_compare_data()
